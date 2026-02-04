293df325a9dd89a2d9d72dd6e1dd6e84c8742013
Petr Ungar <petr.ungar@gmail.com>
2026-02-04 15:13:24 +0100

    add dotenv

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { EmbeddingService } from './main/processor/embedding';
 import { StorageService } from './main/processor/storage';
 import * as interactionMonitor from './main/recorder/interaction-monitor';
 import { Screenshot } from './shared/types';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
