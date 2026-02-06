a40c23e1da8159e154735e05bab4a2539e0ba1b7
Filip Kubiš <fkubis@icloud.com>
2026-02-06 14:47:50 +0100

    fix notarize script

--- build/notarize.js
+++ build/notarize.js
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-<<<<<<< HEAD
 require('dotenv').config()
 const { notarize } = require('@electron/notarize')
 
@@ -10,18 +9,6 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename
 
   console.log(`Notarizing ${appName}...`)
-=======
-require('dotenv').config();
-const { notarize } = require('@electron/notarize');
-
-exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;
-  if (electronPlatformName !== 'darwin') return;
-
-  const appName = context.packager.appInfo.productFilename;
-
-  console.log(`Notarizing ${appName}...`);
->>>>>>> a678e79 (Revert "Use built in electron builder notarization")
 
   await notarize({
     appBundleId: 'com.memorylane.app',
@@ -29,14 +16,7 @@ exports.default = async function notarizing(context) {
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_APP_PASSWORD,
     teamId: 'ZN3J54N7AP',
-<<<<<<< HEAD
   })
 
   console.log('Notarization complete.')
 }
-=======
-  });
-
-  console.log('Notarization complete.');
-};
->>>>>>> a678e79 (Revert "Use built in electron builder notarization")
